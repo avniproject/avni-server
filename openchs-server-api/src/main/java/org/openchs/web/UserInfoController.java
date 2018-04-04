87d9d972ae0e02ef55fd2a4b62dceb892d2eaff3
mihirk <mihir.khatwani@gmail.com>
2018-04-04 14:48:15 +0530

    Revert "Fixing User info controller, and a way to get org'"
    This reverts commit ab9c69762075d104fdb685737ad8e73c7c0cf364.

--- openchs-server-api/src/main/java/org/openchs/web/UserInfoController.java
+++ openchs-server-api/src/main/java/org/openchs/web/UserInfoController.java
@@ -4,7 +4,6 @@ import org.openchs.dao.CatchmentRepository;
 import org.openchs.dao.OrganisationRepository;
 import org.openchs.domain.Catchment;
 import org.openchs.domain.Organisation;
-import org.openchs.framework.security.UserContextHolder;
 import org.openchs.web.request.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -31,7 +30,7 @@ public class UserInfoController {
     @PreAuthorize(value = "hasAnyAuthority('user', 'admin')")
     public ResponseEntity<UserInfo> getUserInfo(@RequestParam(value = "catchmentId", required = true) Integer catchmentId) {
         Catchment catchment = this.catchmentRepository.findOne(Long.valueOf(catchmentId));
-        Organisation organisation = UserContextHolder.getUserContext().getOrganisation();
+        Organisation organisation = this.organisationRepository.findOne(catchment.getOrganisationId());
         return new ResponseEntity<>(new UserInfo(catchment.getType(), organisation.getName()), HttpStatus.OK);
     }
 }
