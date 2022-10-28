df9a68537335b36bb35e285dbe762f792a0fcd51
Vivek Singh <petmongrels@gmail.com>
2022-10-28 17:46:37 +0530

    #481 create permanent password based on org settings. new settings added. refactoring to use OrganisationConfig instead of JSON object.

--- avni-server-api/src/main/resources/database/createBaseLocalTestData.sql
+++ avni-server-api/src/main/resources/database/createBaseLocalTestData.sql
@@ -1,3 +1,11 @@
+insert into organisation_config (uuid, organisation_id, settings, created_by_id, created_date_time, last_modified_date_time, last_modified_by_id)
+values ('760f3a1b-34c0-4801-a522-6f18133c5544',
+        1,
+        '{"languages": ["en"]}'::jsonb, (select id from users where username = 'admin'),
+        current_timestamp,
+        current_timestamp,
+        (select id from users where username = 'admin'));
+
 insert into organisation (name, db_user, uuid, media_directory, username_suffix, schema_name)
 VALUES ('example', 'example', '929031e8-e538-4b60-b5e8-b2b1a8bf3090', 'example', 'example', 'example');
 insert into users (uuid, username, organisation_id, operating_individual_scope, settings, email, phone_number, name, is_org_admin)
