e75312a9c04088d3006b729e36c664c76b3982b4
himeshr <himeshl@samanvayfoundation.org>
2022-12-13 19:25:15 +0530

    #850 | Fix messagingService compilation error due to missing initializations

--- avni-server-api/src/main/java/org/avni/messaging/service/MessageReceiverService.java
+++ avni-server-api/src/main/java/org/avni/messaging/service/MessageReceiverService.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 public class MessageReceiverService {
 
@@ -50,7 +48,7 @@ public class MessageReceiverService {
             return messageReceiver;
         }
 
-        String phoneNumber, fullName;
+        String phoneNumber = null, fullName = null;
         if(messageReceiver.getReceiverType() == ReceiverType.Subject){
             Individual individual = individualService.findById(messageReceiver.getReceiverId());
             phoneNumber = individualService.findPhoneNumber(individual);
