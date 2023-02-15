4c891242a3767e566e5f1c42328301717413e78e
Sachin Kadam <Sachinkadamsk01@gmail.com>
2023-02-15 15:25:55 +0530

    #879 [webapp] | WIP, Visibility of features on avni-webapp (Admin)

--- avni-server-api/src/main/java/org/avni/server/dao/GroupPrivilegeRepository.java
+++ avni-server-api/src/main/java/org/avni/server/dao/GroupPrivilegeRepository.java
@@ -22,14 +22,14 @@ public interface GroupPrivilegeRepository extends ReferenceDataRepository<GroupP
 
     List<GroupPrivilege> findByGroup_Id(Long groupId);
 
-    @Query(value = "select distinct on (subject_type_id, program_id, program_encounter_type_id, encounter_type_id, checklist_detail_id) * \n" +
+    @Query(value = "select distinct on (subject_type_id, program_id, program_encounter_type_id, encounter_type_id, checklist_detail_id,privilege_id) * \n" +
             "from group_privilege gp \n" +
             "         join user_group ug on ug.group_id = gp.group_id \n" +
             "         join privilege p on gp.privilege_id = p.id \n" +
             "where ug.user_id = :userId \n" +
             "  and gp.is_voided = false \n" +
             "  and ug.is_voided = false \n" +
-            "  and p.name in ('View checklist','View enrolment details','View subject','View visit') \n" +
+            "  and p.name in ('View checklist','View enrolment details','View subject','View visit','Edit organisation configuration', 'Delete organisation configuration') \n" +
             "  and allow = true", nativeQuery = true)
     List<GroupPrivilege> getAllAllowPrivilegesForUser(Long userId);
 
