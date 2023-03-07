9109b9ac1443e8947b5759c197a17cea37b7dcdb
Vinay Venu <ijkvinay@gmail.com>
2023-03-07 14:44:52 +0530

    #883 | Ensure erorrs handled by ResponseEntityExceptionHandler are also logged in Bugsnag ErrorInterceptors handle almost all exceptions since it defines a handler for Exception.class. However, there are specific exceptions handled by its parent - ResponseEntityExceptionHandler.handleException. This is a final method, so to change the behaviour of logging of this method, we introduced logging to handleExceptionInternal()

--- avni-server-api/src/main/java/org/avni/server/web/ErrorInterceptors.java
+++ avni-server-api/src/main/java/org/avni/server/web/ErrorInterceptors.java
@@ -7,15 +7,30 @@ import org.avni.server.framework.security.UserContextHolder;
 import org.avni.server.util.BadRequestError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
@@ -25,6 +40,9 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ErrorInterceptors extends ResponseEntityExceptionHandler {
+    private Exception ex;
+    private WebRequest request;
+
     class ApiError {
         private String field;
         private String message;
@@ -79,10 +97,20 @@ public class ErrorInterceptors extends ResponseEntityExceptionHandler {
         } else if (e instanceof ResourceNotFoundException) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         } else {
-            reportToBugsnag(e);
-            log(e);
+            logAndReportToBugsnag(e);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         }
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        logAndReportToBugsnag(ex);
+        return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    private void logAndReportToBugsnag(Exception e) {
+        reportToBugsnag(e);
+        log(e);
     }
 
     private void log(Exception e) {
