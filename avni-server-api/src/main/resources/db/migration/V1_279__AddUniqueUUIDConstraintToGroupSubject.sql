d9f936c08a5cf3f0610dea574fb10e4c5edc2141
Joy A <itsjoy@gmail.com>
2023-03-17 11:05:15 +0530

    #516 | Add delete duplicates query to migration before applying constraint

--- avni-server-api/src/main/resources/db/migration/V1_279__AddUniqueUUIDConstraintToGroupSubject.sql
+++ avni-server-api/src/main/resources/db/migration/V1_279__AddUniqueUUIDConstraintToGroupSubject.sql
@@ -1,2 +1,18 @@
+delete
+from group_subject
+where id in
+      (select id
+       from (select id,
+                    rank() over (partition by group_subject_id, member_subject_id, is_voided order by id desc) rnk
+             from group_subject
+             where uuid in (
+                 -- Duplicate uuids query
+                 select uuid
+                 from (select uuid, count(*)
+                       from group_subject
+                       group by organisation_id, uuid, group_subject_id, member_subject_id, is_voided
+                       having count(*) > 1) duplicate_uuids)) id_of_duplicate_uuids
+       where rnk > 1);
+
 ALTER TABLE group_subject
   ADD UNIQUE (uuid, organisation_id);
