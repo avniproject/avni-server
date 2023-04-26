2537c101c266a3a80787015db030d302054a3c04
himeshr <himeshl@samanvayfoundation.org>
2023-04-26 15:07:04 +0530

    #000 | Add useful sql query to fetch Open DB connections info

--- avni-server-api/src/main/resources/database/usefulQueries.sql
+++ avni-server-api/src/main/resources/database/usefulQueries.sql
@@ -335,6 +335,17 @@ WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes';
 -- See what queries are in waiting to acquire locks
 SELECT relation::regclass, * FROM pg_locks WHERE NOT GRANTED;
 
+-- See number of DB Connections
+
+select max_conn,used,res_for_super,max_conn-used-res_for_super res_for_normal
+from
+    (select count(*) used from pg_stat_activity) t1,
+    (select setting::int res_for_super from pg_settings where name=$$superuser_reserved_connections$$) t2,
+    (select setting::int max_conn from pg_settings where name=$$max_connections$$) t3;
+
+-- Group open db connections by backend name
+SELECT datname, numbackends FROM pg_stat_database;
+
 -- See what processes are blocking what queries (these only find row-level locks, not object-level locks)
 SELECT blocked_locks.pid     AS blocked_pid,
        blocked_activity.usename  AS blocked_user,
