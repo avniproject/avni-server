d72b1462ed8a30f51fd851dfc903c6eb17077191
Joy A <itsjoy@gmail.com>
2023-07-07 12:51:14 +0530

    avniproject/avni-product#1328 | Return archived row count from archive_sync_telemetry

--- avni-server-api/src/main/resources/db/migration/V1_298__ArchiveSyncTelemetryFunction.sql
+++ avni-server-api/src/main/resources/db/migration/V1_298__ArchiveSyncTelemetryFunction.sql
@@ -1,10 +1,13 @@
 DROP FUNCTION IF EXISTS archive_sync_telemetry(olderthan date);
 CREATE FUNCTION archive_sync_telemetry(olderthan date) RETURNS bigint AS $$
+DECLARE
+    archived_row_count bigint;
 BEGIN
     CREATE TABLE IF NOT EXISTS public.sync_telemetry_history (LIKE public.sync_telemetry INCLUDING ALL);
     PERFORM enable_rls_on_tx_table('sync_telemetry_history');
     INSERT INTO public.sync_telemetry_history SELECT * from public.sync_telemetry WHERE sync_start_time < olderthan;
     DELETE FROM public.sync_telemetry WHERE sync_start_time < olderthan;
-    RETURN 1;
+    GET DIAGNOSTICS archived_row_count = ROW_COUNT;
+    RETURN archived_row_count;
 END;
 $$ LANGUAGE plpgsql;
