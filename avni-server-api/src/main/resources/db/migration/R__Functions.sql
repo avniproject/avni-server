e8bfb6672f844e9f2441186edd500f6da82bc084
Joy A <itsjoy@gmail.com>
2023-07-07 13:59:08 +0530

    avniproject/avni-product#1328 | Move archive_sync_telemetry to R__Functions

--- avni-server-api/src/main/resources/db/migration/R__Functions.sql
+++ avni-server-api/src/main/resources/db/migration/R__Functions.sql
@@ -167,3 +167,20 @@ BEGIN
     return true;
 END
 $$;
+
+-- function to archive records from sync_telemetry
+CREATE OR REPLACE FUNCTION archive_sync_telemetry(olderthan date) RETURNS bigint
+    LANGUAGE plpgsql
+AS
+$$
+DECLARE
+    archived_row_count bigint;
+BEGIN
+    CREATE TABLE IF NOT EXISTS public.sync_telemetry_history (LIKE public.sync_telemetry INCLUDING ALL);
+    PERFORM enable_rls_on_tx_table('sync_telemetry_history');
+    INSERT INTO public.sync_telemetry_history SELECT * from public.sync_telemetry WHERE sync_start_time < olderthan;
+    DELETE FROM public.sync_telemetry WHERE sync_start_time < olderthan;
+    GET DIAGNOSTICS archived_row_count = ROW_COUNT;
+    RETURN archived_row_count;
+END;
+$$;
