9d314edab30bfa83d3336b9a869e7019af46e576
himeshr <himeshl@samanvayfoundation.org>
2023-07-27 14:54:25 +0530

    avniproject/avni-webapp#997 | Refactor method name to findByUser_IdAndGroup_IdAndIsVoidedFalse

--- avni-server-api/src/main/java/org/avni/server/dao/UserGroupRepository.java
+++ avni-server-api/src/main/java/org/avni/server/dao/UserGroupRepository.java
@@ -32,7 +32,7 @@ public interface UserGroupRepository extends ReferenceDataRepository<UserGroup> 
         throw new UnsupportedOperationException("No field 'name' in UserGroup.");
     }
 
-    UserGroup findByUserIDAndGroupIdAndIsVoidedFalse(Long userId, Long groupId);
+    UserGroup findByUser_IdAndGroup_IdAndIsVoidedFalse(Long userId, Long groupId);
 
     List<UserGroup> findByGroup_IdAndIsVoidedFalse(Long groupId);
 
