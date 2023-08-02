32d669035480fbe4cae4f8e4d9f9b336aa35ea4a
himeshr <himeshl@samanvayfoundation.org>
2023-08-02 18:58:04 +0530

    avniproject/avni-client#1060 | Exclude non-subjectType group privileges from sync to mobile device

--- avni-server-api/src/main/java/org/avni/server/dao/GroupPrivilegeRepository.java
+++ avni-server-api/src/main/java/org/avni/server/dao/GroupPrivilegeRepository.java
@@ -1,16 +1,24 @@
 package org.avni.server.dao;
 
+import org.avni.server.domain.CHSEntity;
 import org.avni.server.domain.accessControl.GroupPrivilege;
+import org.joda.time.DateTime;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "groupPrivilege", path = "groupPrivilege")
 
-public interface GroupPrivilegeRepository extends ReferenceDataRepository<GroupPrivilege>, FindByLastModifiedDateTime<GroupPrivilege> {
+public interface GroupPrivilegeRepository extends ReferenceDataRepository<GroupPrivilege> {
     default GroupPrivilege findByName(String name) {
         throw new UnsupportedOperationException("No field 'name' in GroupPrivilege.");
     }
@@ -30,4 +38,23 @@ public interface GroupPrivilegeRepository extends ReferenceDataRepository<GroupP
             "  and ug.is_voided = false\n" +
             "  and allow = true", nativeQuery = true)
     List<GroupPrivilege> getAllAllowedPrivilegesForUser(Long userId);
+
+    @RestResource(path = "lastModified", rel = "lastModified")
+    Page<GroupPrivilege> findBySubjectTypeIsNotNullAndLastModifiedDateTimeIsBetweenOrderByLastModifiedDateTimeAscIdAsc(
+            @Param("lastModifiedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date lastModifiedDateTime,
+            @Param("now") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date now,
+            Pageable pageable);
+
+    default Page<GroupPrivilege> findByLastModifiedDateTimeIsBetweenOrderByLastModifiedDateTimeAscIdAsc(
+            @Param("lastModifiedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime lastModifiedDateTime,
+            @Param("now") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime now,
+            Pageable pageable) {
+        return findBySubjectTypeIsNotNullAndLastModifiedDateTimeIsBetweenOrderByLastModifiedDateTimeAscIdAsc(CHSEntity.toDate(lastModifiedDateTime), CHSEntity.toDate(now), pageable);
+    }
+
+    boolean existsByLastModifiedDateTimeGreaterThan(@Param("lastModifiedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) Date lastModifiedDateTime);
+
+    default boolean existsByLastModifiedDateTimeGreaterThan(DateTime lastModifiedDateTime) {
+        return existsByLastModifiedDateTimeGreaterThan(lastModifiedDateTime == null ? null : lastModifiedDateTime.toDate());
+    }
 }
