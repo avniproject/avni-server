5a867393808a52490c56089452198a8149ebf005
Vivek Singh <petmongrels@gmail.com>
2023-08-08 19:22:55 +0530

    avniproject/avni-webapp#1019 - function to delete just one table

--- avni-server-api/src/main/java/org/avni/server/dao/OrganisationConfigRepository.java
+++ avni-server-api/src/main/java/org/avni/server/dao/OrganisationConfigRepository.java
@@ -2,12 +2,10 @@ package org.avni.server.dao;
 
 import org.avni.server.domain.OrganisationConfig;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "organisationConfig", path = "organisationConfig")
-
 public interface OrganisationConfigRepository extends ReferenceDataRepository<OrganisationConfig>, FindByLastModifiedDateTime<OrganisationConfig> {
     default OrganisationConfig findByName(String name) {
         throw new UnsupportedOperationException("No field 'name' in Location. Field 'title' not unique.");
