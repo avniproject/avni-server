d7a47b0408e4add4bdb56b50a29c137bc7741429
himeshr <himeshl@samanvayfoundation.org>
2023-08-30 15:30:50 +0530

    avniproject/avni-security#31 | Override save in TransactionalDataRepository to avoid duplication conflict

--- avni-server-api/src/main/java/org/avni/server/dao/TransactionalDataRepository.java
+++ avni-server-api/src/main/java/org/avni/server/dao/TransactionalDataRepository.java
@@ -3,10 +3,15 @@ package org.avni.server.dao;
 import org.avni.server.domain.CHSEntity;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @NoRepositoryBean
 public interface TransactionalDataRepository<T extends CHSEntity> extends CHSRepository<T>, CustomCHSJpaRepository<T, Long>, JpaSpecificationExecutor<T>{
     default T findOne(Long id) {
         return findById(id).orElse(null);
     }
+
+    @Override
+    @RestResource(exported = false)
+    <S extends T> S save(S entity);
 }
