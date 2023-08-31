de8dd9c7119a70eb3b0ea55e72771747d0bbc922
Maha Lakshme S <mahalakshme1994@gmail.com>
2023-08-31 09:35:57 +0530

    Revert "avniproject/avni-models#43 | Add logging to debug"
    This reverts commit 292b2d2927d791220be1879c4df04c65f3fc6650.

--- avni-server-api/src/main/java/org/avni/server/service/ObservationService.java
+++ avni-server-api/src/main/java/org/avni/server/service/ObservationService.java
@@ -334,8 +334,6 @@ public class ObservationService {
                 observationContract.setValue(this.constructObservations(new ObservationCollection(values)));
             }
         } else {
-            logObject(entry.getValue(), "getObservationContract:entry.getValue():%s:::");
-            logObject(entry.getKey(), "getObservationContract:entry.getKey():%s:::");
             observationContract.setValue(entry.getValue());
         }
         return observationContract;
