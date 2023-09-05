ccee8875738a027b4c29b194e7f7f51b6ca75166
Vivek Singh <petmongrels@gmail.com>
2023-09-05 19:07:40 +0530

    #00 - syntactic changes

--- avni-server-api/src/main/java/org/avni/server/service/UserSubjectAssignmentService.java
+++ avni-server-api/src/main/java/org/avni/server/service/UserSubjectAssignmentService.java
@@ -146,11 +146,11 @@ public class UserSubjectAssignmentService implements NonScopeAwareService {
     public UserSubjectAssignment save(UserSubjectAssignment userSubjectAssignment) throws ValidationException {
         if (!userSubjectAssignment.getSubject().getSubjectType().isGroup()) {
             List<GroupSubject> groupSubjects = groupSubjectRepository.findAllByMemberSubject(userSubjectAssignment.getSubject());
-            List<Long> directlyAssignableGroupIds = groupSubjects.stream().filter(groupSubject -> groupSubject.getGroupSubject().getSubjectType().isDirectlyAssignable()).map(groupSubject1 -> groupSubject1.getGroupSubject().getId()).collect(Collectors.toList());
-            List<UserSubjectAssignment> userSubjectAssignmentsForGroups = userSubjectAssignmentRepository.findUserSubjectAssignmentByUserIsNotAndSubject_IdIn(userSubjectAssignment.getUser(), directlyAssignableGroupIds);
+            List<Long> directlyAssignableGroups = groupSubjects.stream().filter(groupSubject -> groupSubject.getGroupSubject().getSubjectType().isDirectlyAssignable()).map(groupSubject1 -> groupSubject1.getGroupSubject().getId()).collect(Collectors.toList());
+            List<UserSubjectAssignment> userSubjectAssignmentsForGroups = userSubjectAssignmentRepository.findUserSubjectAssignmentByUserIsNotAndSubject_IdIn(userSubjectAssignment.getUser(), directlyAssignableGroups);
             if (userSubjectAssignmentsForGroups.size() > 0) {
                 String subjectsAssignedToDifferentUser = userSubjectAssignmentsForGroups.stream().map(x -> x.getSubject().getUuid()).collect(Collectors.joining(","));
-                throw new ValidationException(String.format("This subject %s, is assigned to group(s) - %s who are not assigned to the same user", userSubjectAssignment.getSubject().getFullName(), subjectsAssignedToDifferentUser));
+                throw new ValidationException(String.format("This subject %s, is member of group(s) - %s who are not assigned to the same user", userSubjectAssignment.getSubject().getFullName(), subjectsAssignedToDifferentUser));
             }
         }
         userSubjectAssignmentRepository.save(userSubjectAssignment);
