5d479d1a8575cff8f91c1f50b18ffafd6b1a49d2
Vinay Venu <ijkvinay@gmail.com>
2023-09-08 00:11:56 +0530

    avniproject/avni-security#17 | Add Samesite LAX to all cookies

--- avni-server-api/src/main/java/org/avni/server/framework/tomcat/TomcatContainerCustomizer.java
+++ avni-server-api/src/main/java/org/avni/server/framework/tomcat/TomcatContainerCustomizer.java
@@ -1,7 +1,10 @@
 package org.avni.server.framework.tomcat;
 
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
@@ -18,9 +21,18 @@ public class TomcatContainerCustomizer implements WebServerFactoryCustomizer<Tom
                 connector.setScheme("https");
                 connector.setProxyPort(443);
             });
+            factory.addContextCustomizers(sameSiteCookiesConfig());
             LOGGER.info("Enabled secure scheme (https).");
         } else {
             LOGGER.warn("Could not change protocol scheme because Tomcat is not used as servlet container.");
         }
+    }
+
+    public TomcatContextCustomizer sameSiteCookiesConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.LAX.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
     }
 }
