1e909bdd8ba980ed38a2281383f95fb8e6c6072e
Vivek Singh <petmongrels@gmail.com>
2023-10-06 13:17:19 +0530

    #621 - added check for missing enums and returning no change for all them

--- avni-server-api/src/main/java/org/avni/server/dao/sync/SyncEntityName.java
+++ avni-server-api/src/main/java/org/avni/server/dao/sync/SyncEntityName.java
@@ -5,6 +5,7 @@ import org.apache.commons.collections.ListUtils;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public enum SyncEntityName {
     ResetSync,
@@ -35,7 +36,7 @@ public enum SyncEntityName {
     Form,
     FormMapping,
     EncounterType,
-    Rule, IndividualRelation, IndividualRelationGenderMapping, IndividualRelationshipType, Program, ProgramOutcome, Gender, Concept, ProgramConfig, Video, SubjectType, ChecklistItemDetail, FormElementGroup, FormElement, ConceptAnswer, Groups, Translation, PlatformTranslation, OrganisationConfig, IdentifierSource, MyGroups, GroupPrivileges, Extension, GroupRole, LocationHierarchy, ReportCard, GroupDashboard, ApprovalStatus, DashboardSectionCardMapping, DashboardFilter, DashboardSection, Dashboard, News, Task, TaskType, UserInfo, Privilege, TaskStatus, StandardReportCardType, TaskUnAssignment, UserSubjectAssignment, Documentation, DocumentationItem, EntityApprovalStatus, ChecklistItemEntityApprovalStatus, MenuItem, VideoTelemetric;
+    Rule, IndividualRelation, IndividualRelationGenderMapping, IndividualRelationshipType, Program, ProgramOutcome, Gender, Concept, ProgramConfig, Video, SubjectType, ChecklistItemDetail, FormElementGroup, FormElement, ConceptAnswer, Groups, Translation, PlatformTranslation, OrganisationConfig, IdentifierSource, MyGroups, GroupPrivileges, Extension, GroupRole, LocationHierarchy, ReportCard, GroupDashboard, ApprovalStatus, DashboardSectionCardMapping, DashboardFilter, DashboardSection, Dashboard, News, Task, TaskType, UserInfo, Privilege, TaskStatus, StandardReportCardType, TaskUnAssignment, UserSubjectAssignment, Documentation, DocumentationItem, EntityApprovalStatus, ChecklistItemEntityApprovalStatus, MenuItem;
 
     public static List<SyncEntityName> transactionalEntities = ListUtils.unmodifiableList(Arrays.asList(Subject, Encounter, Comment, ProgramEncounterEntityApprovalStatus, SubjectMigration, ProgramEncounter, Individual, ProgramEnrolment, Encounter, GroupSubject, CommentThread, IndividualRelationship, SubjectEntityApprovalStatus, EncounterEntityApprovalStatus, ProgramEncounterEntityApprovalStatus, ProgramEnrolmentEntityApprovalStatus, ChecklistItemEntityApprovalStatus, SubjectProgramEligibility, Task, Checklist, ChecklistItem, EntityApprovalStatus));
 
@@ -49,5 +50,9 @@ public enum SyncEntityName {
 
     public boolean nameEquals(String otherName) {
         return this.name().equals(otherName);
+    }
+
+    public static boolean existsAsEnum(String str) {
+        return Stream.of(SyncEntityName.values()).anyMatch(v -> v.name().equals(str));
     }
 }
