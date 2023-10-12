53b63fa5f5070c18e04a1ddc7253996ee422200b
Vivek Singh <petmongrels@gmail.com>
2023-10-12 14:58:31 +0530

    #000 - pg dump/restore utilities

--- makefiles/externalDB.mk
+++ makefiles/externalDB.mk
@@ -1,4 +1,13 @@
-restore_staging_dump:
+restore-org-dump:
+ifndef dumpFile
+	@echo "Provde the dumpFile variable"
+	exit 1
+else
+	make _clean_db _build_db database=avni_org
+	psql -U openchs -d avni_org < $(dumpFile)
+endif
+
+restore-staging-dump:
 ifndef dumpFile
 	@echo "Provde the dumpFile variable"
 	exit 1
@@ -7,7 +16,7 @@ else
 	psql -U openchs -d avni_staging < $(dumpFile)
 endif
 
-restore_staging_dump_release_branch:
+restore-staging-dump-release-branch:
 ifndef dumpFile
 	@echo "Provde the dumpFile variable"
 	exit 1
@@ -16,19 +25,23 @@ else
 	psql -U openchs -d avni_staging_released < $(dumpFile)
 endif
 
-create_local_staging_db_user: ## Creates new implementation db user in local staging database
+create-local-db-impl-user: ## Creates new implementation db user in local staging database
 ifndef user
 	@echo "Provde the variable user"
 	exit 1
 endif
-	-psql -U $(su) -d avni_staging -c "select create_db_user('$(user)', 'password')"
-	-psql -U $(su) -d avni_staging -c "select create_implementation_schema('$(user)', '$(user)')"
-	-psql -U $(su) -d avni_staging -c "grant all privileges on all tables in schema $(user) to $(user)"
+ifndef db
+	@echo "Provde the variable db"
+	exit 1
+endif
+	-psql -U $(su) -d $(db) -c "select create_db_user('$(user)', 'password')"
+	-psql -U $(su) -d $(db) -c "select create_implementation_schema('$(user)', '$(user)')"
+	-psql -U $(su) -d $(db) -c "grant all privileges on all tables in schema $(user) to $(user)"
 
-create_all_local_staging_db_user: ## Creates all implementation db users in local staging database
+create-all-local-staging-db-user: ## Creates all implementation db users in local staging database
 	-psql -U $(su) -d avni_staging -c "select create_db_user(db_user, 'password') from organisation where is_voided = false and id <> 1"
 
-create_local_staging_db_user_release_branch:
+create-local-staging-db-user-release-branch:
 ifndef user
 	@echo "Provde the variable"
 	exit 1
