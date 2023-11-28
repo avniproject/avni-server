7aa450f994c77ab11932df71ae62fab68e090d55
himeshr <himeshl@samanvayfoundation.org>
2023-11-28 18:03:28 +0530

    #000 | Add locks monitor query to usefulQueries.sql

--- avni-server-api/src/main/resources/database/usefulQueries.sql
+++ avni-server-api/src/main/resources/database/usefulQueries.sql
@@ -368,6 +368,29 @@ WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes';
 -- See what queries are in waiting to acquire locks
 SELECT relation::regclass, * FROM pg_locks WHERE NOT GRANTED;
 
+-- Create lock_monitor and use it to find blocking PIDs
+CREATE OR REPLACE VIEW lock_monitor AS(
+                                      SELECT
+                                          COALESCE(blockingl.relation::regclass::text,blockingl.locktype) as locked_item,
+                                          now() - blockeda.query_start AS waiting_duration, blockeda.pid AS blocked_pid,
+                                          blockeda.query as blocked_query, blockedl.mode as blocked_mode,
+                                          blockinga.pid AS blocking_pid, blockinga.query as blocking_query,
+                                          blockingl.mode as blocking_mode
+                                      FROM pg_catalog.pg_locks blockedl
+                                               JOIN pg_stat_activity blockeda ON blockedl.pid = blockeda.pid
+                                               JOIN pg_catalog.pg_locks blockingl ON(
+                                              ( (blockingl.transactionid=blockedl.transactionid) OR
+                                                (blockingl.relation=blockedl.relation AND blockingl.locktype=blockedl.locktype)
+                                                  ) AND blockedl.pid != blockingl.pid)
+                                               JOIN pg_stat_activity blockinga ON blockingl.pid = blockinga.pid
+                                          AND blockinga.datid = blockeda.datid
+                                      WHERE NOT blockedl.granted
+                                        AND blockinga.datname = current_database()
+                                          );
+
+-- To show locks and blocking PIDs
+SELECT * from lock_monitor;
+
 -- See number of DB Connections
 
 select max_conn,used,res_for_super,max_conn-used-res_for_super res_for_normal
