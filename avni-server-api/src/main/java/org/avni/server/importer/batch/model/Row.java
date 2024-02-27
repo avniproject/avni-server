bcf90db3a715bbfa7e0359d889a9d517eec84a5c
Vinay Venu <ijkvinay@gmail.com>
2024-02-27 11:08:59 +0530

    #695 | Verify that csv cells are sanitized

--- avni-server-api/src/main/java/org/avni/server/importer/batch/model/Row.java
+++ avni-server-api/src/main/java/org/avni/server/importer/batch/model/Row.java
@@ -2,7 +2,6 @@ package org.avni.server.importer.batch.model;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
@@ -12,10 +11,12 @@ public class Row extends HashMap<String, String> {
 
     public static final Pattern TRUE_VALUE = Pattern.compile("y|yes|true|1", Pattern.CASE_INSENSITIVE);
     private final String[] headers;
+    private final String[] values;
 
     public Row(String[] headers, String[] values) {
         this.headers = headers;
-        IntStream.range(0, values.length).forEach(index -> this.put(headers[index], values[index]));
+        this.values = values;
+        IntStream.range(0, values.length).forEach(index -> this.put(headers[index], values[index].trim()));
     }
 
     public String[] getHeaders() {
@@ -24,8 +25,8 @@ public class Row extends HashMap<String, String> {
 
     @Override
     public String toString() {
-        return Arrays.stream(headers)
-                .map(header -> format("\"%s\"", get(header) != null ? get(header) : ""))
+        return IntStream.range(0, headers.length)
+                .mapToObj(index -> index < values.length? format("\"%s\"", values[index]): "\"\"")
                 .reduce((c1, c2) -> format("%s,%s", c1, c2))
                 .get();
     }
