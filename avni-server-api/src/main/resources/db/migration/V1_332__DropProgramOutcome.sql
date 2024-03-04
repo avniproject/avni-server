fc25c18edfbd6a2174aa70b03114d10095102149
Vivek Singh <petmongrels@gmail.com>
2024-03-04 16:45:11 +0530

    #520 - cannot drop program outcome table and column due to existing views referring to them

--- avni-server-api/src/main/resources/db/migration/V1_332__DropProgramOutcome.sql
+++ avni-server-api/src/main/resources/db/migration/V1_332__DropProgramOutcome.sql
@@ -1,2 +1,2 @@
-alter table program_enrolment drop column program_outcome_id;
-drop table if exists program_outcome;
+-- cannot drop column or program_outcome table because it is used in reporting views
+alter table program_enrolment drop CONSTRAINT program_enrolment_program_outcome;
