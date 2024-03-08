0dfe3e729cd0fc7c50bb1d61a5bb6a7676b2ce6c
vedfordev <vedforworks@gmail.com>
2024-03-08 12:39:58 +0530

    avniproject/avni-webapp#1140 | added environment variable for name and url separately

--- avni-server-api/src/main/java/org/avni/server/web/util/Configuration.java
+++ avni-server-api/src/main/java/org/avni/server/web/util/Configuration.java
@@ -1,29 +1,13 @@
 package org.avni.server.web.util;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.avni.server.web.OrganisationConfigController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Component
+@ConfigurationProperties(prefix = "avni")
 public class Configuration {
-    @Value("${avni.reportingSystems}")
-    private String report;
     private List<ReportingSystem> reportingSystems;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-    private static final Logger logger = LoggerFactory.getLogger(OrganisationConfigController.class);
-
-    public void setReport(String report) {
-        System.out.println(report);
-        this.report = report;
-    }
 
     public List<ReportingSystem> getReportingSystems() {
         return reportingSystems;
@@ -31,15 +15,5 @@ public class Configuration {
 
     public void setReportingSystems(List<ReportingSystem> reportingSystems) {
         this.reportingSystems = reportingSystems;
-    }
-
-    @PostConstruct
-    public void addReportingSystems() {
-        try {
-            logger.info(String.format("reporting system getting from environemt %s", report));
-            reportingSystems = objectMapper.readValue(report, objectMapper.getTypeFactory().constructCollectionType(List.class, ReportingSystem.class));
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
     }
 }
