1ec93cf59c11336961d5577d89646192640c2aa8
Vivek Singh <petmongrels@gmail.com>
2024-03-11 18:34:33 +0530

    avniproject/avni-server#1152 - split controllers into web and sync. added entity listener to track audit fields. removed code for manual management of audit info.

---
+++ avni-server-api/src/main/java/org/avni/server/web/rules/RuleFailureTelemetryController.java
@@ -0,0 +1,59 @@
+package org.avni.server.web.rules;
+
+import org.avni.server.dao.RuleFailureTelemetryRepository;
+import org.avni.server.domain.Organisation;
+import org.avni.server.domain.RuleFailureTelemetry;
+import org.avni.server.domain.User;
+import org.avni.server.framework.security.UserContextHolder;
+import org.avni.server.web.RestControllerResourceProcessor;
+import org.avni.server.web.request.RuleFailureTelemetryRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.transaction.Transactional;
+
+@RestController
+public class RuleFailureTelemetryController implements RestControllerResourceProcessor<RuleFailureTelemetry> {
+    private final RuleFailureTelemetryRepository ruleFailureTelemetryRepository;
+
+    @Autowired
+    public RuleFailureTelemetryController(RuleFailureTelemetryRepository ruleFailureTelemetryRepository) {
+        this.ruleFailureTelemetryRepository = ruleFailureTelemetryRepository;
+    }
+
+    @RequestMapping(value = "ruleFailureTelemetry", method = RequestMethod.GET)
+    @PreAuthorize(value = "hasAnyAuthority('user')")
+    public PagedResources<Resource<RuleFailureTelemetry>> getEmpty(Pageable pageable) {
+        return empty(pageable);
+    }
+
+    @RequestMapping(value = "/ruleFailureTelemetry", method = RequestMethod.POST)
+    @Transactional
+    @PreAuthorize(value = "hasAnyAuthority('user')")
+    public void save(@RequestBody RuleFailureTelemetryRequest request) {
+        User user = UserContextHolder.getUserContext().getUser();
+        Organisation organisation = UserContextHolder.getUserContext().getOrganisation();
+        RuleFailureTelemetry ruleFailureTelemetry = new RuleFailureTelemetry();
+        ruleFailureTelemetry.setUser(user);
+        ruleFailureTelemetry.setOrganisationId(organisation.getId());
+        ruleFailureTelemetry.setErrorMessage(request.getErrorMessage());
+        ruleFailureTelemetry.setIndividualUuid(request.getIndividualUuid());
+        ruleFailureTelemetry.setStacktrace(request.getStacktrace());
+        ruleFailureTelemetry.setRuleUuid(request.getRuleUuid());
+        ruleFailureTelemetry.setErrorDateTime(request.getErrorDateTime());
+        ruleFailureTelemetry.setSourceType(request.getSourceType());
+        ruleFailureTelemetry.setSourceId(request.getSourceId());
+        ruleFailureTelemetry.setEntityType(request.getEntityType());
+        ruleFailureTelemetry.setEntityId(request.getEntityId());
+        ruleFailureTelemetry.setAppType(request.getAppType());
+        ruleFailureTelemetry.setClosed(request.getClosed());
+        ruleFailureTelemetryRepository.save(ruleFailureTelemetry);
+    }
+}
