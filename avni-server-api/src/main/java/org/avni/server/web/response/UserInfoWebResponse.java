5d714c7c9479641e6b45c6cd012f2750f4badf80
Vivek Singh <petmongrels@gmail.com>
2024-05-03 16:53:29 +0530

    avniproject/avni-webapp#1147 - include organisation category info in web/userInfo response

--- avni-server-api/src/main/java/org/avni/server/web/response/UserInfoWebResponse.java
+++ avni-server-api/src/main/java/org/avni/server/web/response/UserInfoWebResponse.java
@@ -3,11 +3,13 @@ package org.avni.server.web.response;
 import org.avni.server.domain.JsonObject;
 import org.avni.server.domain.Organisation;
 import org.avni.server.domain.User;
+import org.avni.server.domain.metadata.OrganisationCategory;
 import org.avni.server.web.request.UserInfoContract;
 
 import java.util.List;
 
 public class UserInfoWebResponse extends UserInfoContract {
+    private OrganisationCategory organisationCategory;
     private long lastSessionTime;
     private boolean hasAllPrivileges;
     private List<UserPrivilegeWebResponse> privileges;
@@ -27,15 +29,17 @@ public class UserInfoWebResponse extends UserInfoContract {
             response.setOrganisationId(contextOrganisation.getId());
             response.setOrganisationName(contextOrganisation.getName());
             response.setUsernameSuffix(contextOrganisation.getEffectiveUsernameSuffix());
+            response.organisationCategory = contextOrganisation.getCategory();
         }
         return response;
     }
 
-    public UserInfoWebResponse(String username, String orgName, Long orgId, String usernameSuffix, JsonObject settings, String name, String catchmentName, JsonObject syncSettings, List<UserPrivilegeWebResponse> privileges, boolean hasAllPrivileges, long lastSessionTime) {
+    public UserInfoWebResponse(String username, String orgName, Long orgId, String usernameSuffix, JsonObject settings, String name, String catchmentName, JsonObject syncSettings, List<UserPrivilegeWebResponse> privileges, boolean hasAllPrivileges, long lastSessionTime, OrganisationCategory organisationCategory) {
         super(username, orgName, orgId, usernameSuffix, settings, name, catchmentName, syncSettings);
         this.privileges = privileges;
         this.hasAllPrivileges = hasAllPrivileges;
         this.lastSessionTime = lastSessionTime;
+        this.organisationCategory = organisationCategory;
     }
 
     public List<UserPrivilegeWebResponse> getPrivileges() {
@@ -56,5 +60,9 @@ public class UserInfoWebResponse extends UserInfoContract {
 
     public void setLastSessionTime(long lastSessionTime) {
         this.lastSessionTime = lastSessionTime;
+    }
+
+    public OrganisationCategory getOrganisationCategory() {
+        return organisationCategory;
     }
 }
