a1d2564e95caa6fd49481d600b641368a30aba17
Vivek Singh <petmongrels@gmail.com>
2024-05-09 13:48:37 +0530

    #0 - conveinience target

--- makefiles/prod.mk
+++ makefiles/prod.mk
@@ -7,3 +7,10 @@ endif
 
 open-cognito-prod:
 	open https://ap-south-1.console.aws.amazon.com/cognito/v2/idp/user-pools/ap-south-1_DU27AHJvZ/users
+
+open-thumbnails-folder-prod:
+ifndef orgMedia
+	@echo "Provde the orgMedia variable with org media prefix"
+	exit 1
+endif
+	open "https://s3.ap-south-1.amazonaws.com/prod-user-media/${orgMedia}/thumbnails/"
