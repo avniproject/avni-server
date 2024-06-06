b6ff1db03c7b2baa6262c53e65371d165b7b41f8
Vivek Singh <petmongrels@gmail.com>
2024-06-06 14:40:25 +0530

    avniproject/avni-webapp#763 - removed order by id and distinct from search query. removed n+1 query for fetch program details.

--- avni-server-api/src/main/java/org/avni/server/service/IndividualSearchService.java
+++ avni-server-api/src/main/java/org/avni/server/service/IndividualSearchService.java
@@ -4,15 +4,12 @@ import org.avni.server.dao.IndividualRepository;
 import org.avni.server.dao.ProgramEnrolmentRepository;
 import org.avni.server.dao.SubjectSearchRepository;
 import org.avni.server.dao.search.SubjectSearchQueryBuilder;
-import org.avni.server.domain.Program;
-import org.avni.server.web.request.EnrolmentContract;
+import org.avni.server.projection.SearchSubjectEnrolledProgram;
 import org.avni.server.web.request.webapp.search.SubjectSearchRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +18,7 @@ import java.util.stream.Collectors;
 @Service
 public class IndividualSearchService {
     private final SubjectSearchRepository subjectSearchRepository;
-    private ProgramEnrolmentRepository programEnrolmentRepository;
+    private final ProgramEnrolmentRepository programEnrolmentRepository;
 
     @Autowired
     public IndividualSearchService(SubjectSearchRepository subjectSearchRepository, IndividualRepository individualRepository, ProgramEnrolmentRepository programEnrolmentRepository) {
@@ -29,7 +26,6 @@ public class IndividualSearchService {
         this.programEnrolmentRepository = programEnrolmentRepository;
     }
 
-    @Transactional
     public LinkedHashMap<String, Object> search(SubjectSearchRequest subjectSearchRequest) {
         List<Map<String, Object>> searchResults = subjectSearchRepository.search(subjectSearchRequest, new SubjectSearchQueryBuilder());
         BigInteger totalCount = subjectSearchRepository.getTotalCount(subjectSearchRequest, new SubjectSearchQueryBuilder());
@@ -38,26 +34,22 @@ public class IndividualSearchService {
 
     private LinkedHashMap<String, Object> constructIndividual(List<Map<String, Object>> individualList, BigInteger totalCount) {
         LinkedHashMap<String, Object> recordsMap = new LinkedHashMap<String, Object>();
+        List<Long> individualIds = individualList.stream()
+                .map(individualRecord -> Long.valueOf((Integer) individualRecord.get("id")))
+                .collect(Collectors.toList());
+        List<SearchSubjectEnrolledProgram> searchSubjectEnrolledPrograms = programEnrolmentRepository.findActiveEnrolmentsByIndividualIds(individualIds);
+
         List<Map<String, Object>> listOfRecords = individualList.stream()
                 .peek(individualRecord -> {
                     Long individualId = Long.valueOf((Integer) individualRecord.get("id"));
-                    individualRecord.put("enrolments", constructEnrolments(individualId));
+                    individualRecord.put("enrolments", searchSubjectEnrolledPrograms.stream()
+                            .filter(x -> x.getId().equals(individualId))
+                            .map(SearchSubjectEnrolledProgram::getProgram)
+                            .collect(Collectors.toList()));
                 }).collect(Collectors.toList());
         recordsMap.put("totalElements", totalCount);
         recordsMap.put("listOfRecords", listOfRecords);
         return recordsMap;
     }
 
-    private List<EnrolmentContract> constructEnrolments(Long individualId) {
-        List<EnrolmentContract> enrolmentContracts = new ArrayList<>();
-        List<Program> programs = programEnrolmentRepository.findActiveEnrolmentsByIndividualId(individualId);
-
-        programs.stream().forEach(program -> {
-            EnrolmentContract enrolmentContract = new EnrolmentContract();
-            enrolmentContract.setOperationalProgramName(program.getOperationalProgramName());
-            enrolmentContract.setProgramColor(program.getColour());
-            enrolmentContracts.add(enrolmentContract);
-        });
-        return enrolmentContracts;
-    }
 }
