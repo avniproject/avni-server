aaa738cca7c51e92ca2bc7f03d54cb80e2586771
Vivek Singh <petmongrels@gmail.com>
2024-06-19 12:55:04 +0530

    #0 - open a specific file

--- makefiles/prod.mk
+++ makefiles/prod.mk
@@ -1,9 +1,16 @@
-open-user-media-prod:
+open-user-media-base-prod:
 ifndef orgMedia
 	@echo "Provde the orgMedia variable with org media prefix"
 	exit 1
 endif
 	open "https://s3.console.aws.amazon.com/s3/buckets/prod-user-media?region=ap-south-1&prefix=$(orgMedia)/&showversions=false"
+
+open-user-media-prod:
+ifndef filePath
+	@echo "Provde the filePath variable"
+	exit 1
+endif
+	open "https://ap-south-1.console.aws.amazon.com/s3/object/prod-user-media?region=ap-south-1&bucketType=general&prefix=$(filePath)"
 
 open-bulk-uploads-error:
 	open "https://s3.console.aws.amazon.com/s3/buckets/prod-user-media?region=ap-south-1&prefix=bulkuploads/error/$(orgMedia)/"
