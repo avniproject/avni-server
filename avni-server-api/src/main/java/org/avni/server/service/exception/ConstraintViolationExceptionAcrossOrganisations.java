00f325d0d04c6cf008a3271b1505df49d5d6c5b4
himeshr <himeshl@samanvayfoundation.org>
2024-07-12 18:11:55 +0530

    Revert "avniproject/avni-client#1458 | Log constraint violation specifics during entity upsert"
    This reverts commit 77116c823385203b952aadbcab4f32f08e08ddb8.

--- avni-server-api/src/main/java/org/avni/server/service/exception/ConstraintViolationExceptionAcrossOrganisations.java
+++
@@ -1,9 +0,0 @@
-package org.avni.server.service.exception;
-
-import org.hibernate.exception.ConstraintViolationException;
-
-public class ConstraintViolationExceptionAcrossOrganisations extends ConstraintViolationException {
-    public ConstraintViolationExceptionAcrossOrganisations(String message, ConstraintViolationException cve) {
-        super(message, cve.getSQLException(),  cve.getSQL(), cve.getConstraintName());
-    }
-}
