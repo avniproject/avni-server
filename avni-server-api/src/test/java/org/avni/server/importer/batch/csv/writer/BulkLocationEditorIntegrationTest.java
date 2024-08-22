711c2506383c82c4f59e0624bd07a8b1d4e665b2
Vivek Singh <petmongrels@gmail.com>
2024-08-22 13:20:31 +0530

    avniproject/avni-webapp#1306 - fixed scenarios in BulkLocationCreatorIntegrationTest

--- avni-server-api/src/test/java/org/avni/server/importer/batch/csv/writer/BulkLocationEditorIntegrationTest.java
+++ avni-server-api/src/test/java/org/avni/server/importer/batch/csv/writer/BulkLocationEditorIntegrationTest.java
@@ -1,8 +1,9 @@
 package org.avni.server.importer.batch.csv.writer;
 
-import org.avni.server.common.AbstractControllerIntegrationTest;
 import org.avni.server.domain.AddressLevelType;
+import org.avni.server.domain.ConceptDataType;
 import org.avni.server.domain.factory.AddressLevelTypeBuilder;
+import org.avni.server.service.builder.TestConceptService;
 import org.avni.server.service.builder.TestDataSetupService;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -14,9 +15,11 @@ import java.util.UUID;
 
 @Sql(value = {"/tear-down.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(value = {"/tear-down.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-public class BulkLocationEditorIntegrationTest extends AbstractControllerIntegrationTest {
+public class BulkLocationEditorIntegrationTest extends BaseCSVImportTest {
     @Autowired
     private TestDataSetupService testDataSetupService;
+    @Autowired
+    private TestConceptService testConceptService;
 
     @Test
     @Ignore
@@ -26,6 +29,8 @@ public class BulkLocationEditorIntegrationTest extends AbstractControllerIntegra
         AddressLevelType district = new AddressLevelTypeBuilder().name("District").level(3d).withUuid(UUID.randomUUID()).build();
         AddressLevelType state = new AddressLevelTypeBuilder().name("State").level(4d).withUuid(UUID.randomUUID()).build();
         testDataSetupService.saveLocationTypes(Arrays.asList(block, district, state));
+        testConceptService.createCodedConcept("Coded Concept", "Answer 1", "Answer 2");
+        testConceptService.createConcept("Text Concept", ConceptDataType.Text);
 
         setUser(organisationData.getUser().getUsername());
     }
