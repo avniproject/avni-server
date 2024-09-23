9e1d280bb98b9f64230f3e787eca6a9ee9434752
Joy A <itsjoy@gmail.com>
2024-09-23 14:26:53 +0530

    avniproject/avni-webapp#1300 | Fix /locations/search/findAsList to include titleLineage in response

--- avni-server-api/src/main/java/org/avni/server/web/LocationController.java
+++ avni-server-api/src/main/java/org/avni/server/web/LocationController.java
@@ -96,10 +96,10 @@ public class LocationController implements RestControllerResourceProcessor<Addre
 
     @GetMapping(value = "locations/search/findAsList")
     @ResponseBody
-    public List<LocationProjection> findAsList(
+    public List<AddressLevelContractWeb> findAsList(
         @RequestParam(value = "title", defaultValue = "") String title,
         @RequestParam(value = "typeId", required = false) Integer typeId) {
-        return locationRepository.findLocationProjectionByTitleIgnoreCaseAndTypeIdAsList(title, typeId);
+        return addressLevelService.addTitleLineageToLocation(locationRepository.findLocationProjectionByTitleIgnoreCaseAndTypeIdAsList(title, typeId));
     }
 
     @GetMapping(value = "/locations/search/findAllById")
