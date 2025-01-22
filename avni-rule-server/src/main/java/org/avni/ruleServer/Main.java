870674b27ca591a57c63428f9f5b9d011f472cfe
Vivek Singh <petmongrels@gmail.com>
2025-01-22 13:27:04 +0530

    avniproject/avni-webapp#1258 - WIP

--- avni-rule-server/src/main/java/org/avni/ruleServer/Main.java
+++ avni-rule-server/src/main/java/org/avni/ruleServer/Main.java
@@ -3,11 +3,21 @@ package org.avni.ruleServer;
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Paths;
 
+@SpringBootApplication
+@EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class Main {
+    @Autowired
+    public Main() {
+    }
+
     public static void main(String[] args) throws IOException {
         String jsFilePath = "build/resources/js/main.js";
 
