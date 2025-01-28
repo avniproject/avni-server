b2d9012a4a28fdad849ad87c60940f86cee03e0d
Vivek Singh <petmongrels@gmail.com>
2025-01-28 16:02:11 +0530

    avniproject/avni-webapp#1258 - WIP

--- avni-rule-server/src/main/java/org/avni/Main.java
+++ avni-rule-server/src/main/java/org/avni/Main.java
@@ -21,27 +21,11 @@ public class Main {
         ApplicationContext applicationContext = SpringApplication.run(Main.class);
         DashboardRepository dashboardRepository = applicationContext.getBean(DashboardRepository.class);
         Dashboard dashboard = dashboardRepository.findOne(1l);
+        this.initialiseJavaScript();
         System.out.println(dashboard.getName());
     }
 
-    public void main2(String[] args) throws IOException {
-        String projectPath = System.getProperty("user.dir");
-        String jsFilePath = "avni-rule-server/build/resources/js/main.js";
+    private void initialiseJavaScript() throws IOException {
 
-        try (Context context = Context.newBuilder("js")
-                .allowAllAccess(true) // Allow access to file system if needed
-                .option("js.esm-eval-returns-exports", "true") // Enable ECMAScript modules
-                .build()) {
-
-            // Read and execute the main JavaScript file
-            Source source = Source.newBuilder("js", Paths.get(projectPath, jsFilePath).toFile())
-                    .mimeType("application/javascript+module")
-                    .build();
-            context.eval(source);
-
-            // Call a function from the JavaScript code (optional)
-            Value result = context.eval(source);
-            System.out.println(result);
-        }
     }
 }
--- avni-rule-server/src/main/java/org/avni/ruleServer/RuleService.java
+++ avni-rule-server/src/main/java/org/avni/ruleServer/RuleService.java
@@ -1,8 +1,14 @@
 package org.avni.ruleServer;
 
 import org.avni.server.dao.DashboardRepository;
+import org.graalvm.polyglot.Context;
+import org.graalvm.polyglot.Source;
+import org.graalvm.polyglot.Value;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.nio.file.Paths;
 
 @Component
 public class RuleService {
@@ -11,6 +17,27 @@ public class RuleService {
     @Autowired
     public RuleService(DashboardRepository dashboardRepository) {
         this.dashboardRepository = dashboardRepository;
+    }
+    
+    public void init() {
+        String projectPath = System.getProperty("user.dir");
+        String jsFilePath = "avni-rule-server/build/resources/js/main.js";
+
+        Context context = Context.newBuilder("js")
+                .allowAllAccess(true) // Allow access to file system if needed
+                .option("js.esm-eval-returns-exports", "true") // Enable ECMAScript modules
+                .build();
+        Source source = Source.newBuilder("js", Paths.get(projectPath, jsFilePath).toFile())
+                .mimeType("application/javascript+module")
+                .build();
+        Value invokerO = context.eval(source);
+        try (context) {
+            // Read and execute the main JavaScript file
+            
+            
+        } catch (IOException ioException) {
+            throw new RuntimeException(ioException);
+        }
     }
 
     public void getDashboardReportCardsCount() {
