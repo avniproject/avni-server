0a9834f54bbfc1352da3b3e415a1a42dc290887d
himeshr <himeshl@samanvayfoundation.org>
2025-02-19 11:39:17 +0530

    avniproject/avni-server#854 | fix metabase DB connection by specifying port as number and ssl connection settings

--- avni-server-data/src/main/java/org/avni/server/domain/metabase/DatabaseDetails.java
+++ avni-server-data/src/main/java/org/avni/server/domain/metabase/DatabaseDetails.java
@@ -1,35 +1,50 @@
 package org.avni.server.domain.metabase;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DatabaseDetails {
     private String host;
-    private String port;
+    private int port;
     private String dbname;
     private String user;
     private String password;
+    private boolean ssl;
+
+    @JsonProperty("ssl-mode")
+    private String sslMode;
+
+    @JsonProperty("ssl-use-client-auth")
+    private boolean sslUseClientAuth;
+
+    @JsonProperty("tunnel-enabled")
+    private boolean tunnelEnabled;
 
     public DatabaseDetails() {
     }
 
-    public DatabaseDetails(String host, String port, String dbname, String user, String password) {
+    public DatabaseDetails(String host, int port, String dbname, String user, String password) {
         this.host = host;
         this.port = port;
         this.dbname = dbname;
         this.user = user;
         this.password = password;
+        this.ssl = true;
+        this.sslMode = "require";
+        this.sslUseClientAuth = false;
+        this.tunnelEnabled = false;
     }
 
     public DatabaseDetails(AvniDatabase avniDatabase, String dbUser, String dbUserPassword) {
-        this(avniDatabase.getAvniDatabaseServer(), avniDatabase.getAvniDatabasePort(), avniDatabase.getAvniDatabaseName(), dbUser, dbUserPassword);
+        this(avniDatabase.getAvniDatabaseServer(), Integer.parseInt(avniDatabase.getAvniDatabasePort()), avniDatabase.getAvniDatabaseName(), dbUser, dbUserPassword);
     }
 
     public String getHost() {
         return host;
     }
 
-    public String getPort() {
+    public int getPort() {
         return port;
     }
 
@@ -43,5 +58,21 @@ public class DatabaseDetails {
 
     public String getPassword() {
         return password;
+    }
+
+    public boolean isSsl() {
+        return ssl;
+    }
+
+    public String getSslMode() {
+        return sslMode;
+    }
+
+    public boolean isSslUseClientAuth() {
+        return sslUseClientAuth;
+    }
+
+    public boolean isTunnelEnabled() {
+        return tunnelEnabled;
     }
 }
