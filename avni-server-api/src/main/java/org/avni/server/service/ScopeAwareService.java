b8f9e2f25f4a22115c0c9acdec990d0b8eb641a8
Joy A <itsjoy@gmail.com>
2025-04-08 18:11:09 +0530

    #842 | Restrict sync for large catchments

--- avni-server-api/src/main/java/org/avni/server/service/ScopeAwareService.java
+++ avni-server-api/src/main/java/org/avni/server/service/ScopeAwareService.java
@@ -2,10 +2,10 @@ package org.avni.server.service;
 
 import org.avni.server.dao.SyncParameters;
 import org.avni.server.dao.SyncableRepository;
-import org.avni.server.domain.sync.SyncEntityName;
 import org.avni.server.domain.CHSEntity;
 import org.avni.server.domain.SubjectType;
 import org.avni.server.domain.User;
+import org.avni.server.domain.sync.SyncEntityName;
 import org.avni.server.framework.ApplicationContextProvider;
 import org.avni.server.web.validation.ValidationException;
 import org.joda.time.DateTime;
@@ -13,10 +13,14 @@ import org.joda.time.DateTime;
 import java.util.List;
 
 public interface ScopeAwareService<T extends CHSEntity> {
+    int MAX_SUPPORTED_LOCATIONS_IN_CATCHMENT = 65535;
 
     default boolean isChangedBySubjectTypeRegistrationLocationType(User user, DateTime lastModifiedDateTime, Long typeId, SubjectType subjectType, SyncEntityName syncEntityName) {
         AddressLevelService addressLevelService = ApplicationContextProvider.getContext().getBean(AddressLevelService.class);
         List<Long> addressIds = addressLevelService.getAllRegistrationAddressIdsBySubjectType(user.getCatchment(), subjectType);
+        if (addressIds.size() > MAX_SUPPORTED_LOCATIONS_IN_CATCHMENT) {
+            throw new ValidationException("CatchmentTooLarge");
+        }
         return repository().isEntityChanged(new SyncParameters(lastModifiedDateTime, DateTime.now(), typeId, null, null, addressIds, subjectType, user.getSyncSettings(), syncEntityName, user.getCatchment()));
     }
 
