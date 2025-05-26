992e4a483196138cd8317ce33d5737609d0127ec
Joy A <itsjoy@gmail.com>
2025-05-26 10:59:19 +0530

    avniproject/avni-webapp#1480 | Apply privilege check for access to archival config endpoints

--- avni-server-api/src/main/java/org/avni/server/web/ArchivalConfigController.java
+++ avni-server-api/src/main/java/org/avni/server/web/ArchivalConfigController.java
@@ -1,7 +1,9 @@
 package org.avni.server.web;
 
 import org.avni.server.domain.ArchivalConfig;
+import org.avni.server.domain.accessControl.PrivilegeType;
 import org.avni.server.service.ArchivalConfigService;
+import org.avni.server.service.accessControl.AccessControlService;
 import org.avni.server.web.contract.ArchivalConfigContract;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,21 +18,24 @@ public class ArchivalConfigController {
 
     private final ArchivalConfigService archivalConfigService;
 
+    private final AccessControlService accessControlService;
+
     @Autowired
-    public ArchivalConfigController(ArchivalConfigService archivalConfigService) {
+    public ArchivalConfigController(ArchivalConfigService archivalConfigService, AccessControlService accessControlService) {
         this.archivalConfigService = archivalConfigService;
+        this.accessControlService = accessControlService;
     }
 
     @PostMapping(value = "/web/archivalConfig")
     public ResponseEntity<ArchivalConfig> createOrUpdateArchivalConfig(@RequestBody ArchivalConfigContract archivalConfig) {
-        //TODO: Add privilege check once finalised
+        accessControlService.checkPrivilege(PrivilegeType.EditOrganisationConfiguration);
         ArchivalConfig savedConfig = archivalConfigService.saveOrUpdate(archivalConfig);
         return new ResponseEntity<>(savedConfig, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/web/archivalConfig")
     public ResponseEntity<ArchivalConfigContract> getArchivalConfig() {
-        //TODO: Add privilege check once finalised
+        accessControlService.checkPrivilege(PrivilegeType.EditOrganisationConfiguration);
         ArchivalConfig archivalConfig = archivalConfigService.getArchivalConfig();
         return new ResponseEntity<>(archivalConfig != null ? archivalConfigService.toContract(archivalConfig) : null, HttpStatus.OK);
     }
