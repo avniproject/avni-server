2987908cac08fb0d34c266f7452557092b23035c
himeshr <himeshl@samanvayfoundation.org>
2025-06-30 18:57:48 +0530

    avniproject/avni-webapp#1515 | Fix existing tests and new ones for AddressLevelType upsert

--- avni-server-api/src/test/java/org/avni/server/web/AddressLevelTypeControllerUnitTest.java
+++ avni-server-api/src/test/java/org/avni/server/web/AddressLevelTypeControllerUnitTest.java
@@ -1,10 +1,13 @@
 package org.avni.server.web;
 
 import org.avni.server.dao.AddressLevelTypeRepository;
+import org.avni.server.dao.LocationMappingRepository;
 import org.avni.server.dao.LocationRepository;
+import org.avni.server.dao.OrganisationRepository;
 import org.avni.server.domain.AddressLevelType;
 import org.avni.server.service.LocationService;
 import org.avni.server.service.OrganisationConfigService;
+import org.avni.server.service.ResetSyncService;
 import org.avni.server.service.accessControl.AccessControlServiceStub;
 import org.avni.server.web.request.AddressLevelTypeContract;
 import org.junit.Before;
@@ -25,8 +28,13 @@ import static org.mockito.MockitoAnnotations.initMocks;
 public class AddressLevelTypeControllerUnitTest {
     @Mock
     private AddressLevelTypeRepository addressLevelTypeRepository;
-    @Mock
     private LocationService locationService;
+    @Mock
+    OrganisationRepository organisationRepository;
+    @Mock
+    LocationMappingRepository locationMappingRepository;
+    @Mock
+    ResetSyncService resetSyncService;
     @Mock
     private LocationRepository locationRepository;
     @Mock
@@ -41,6 +49,7 @@ public class AddressLevelTypeControllerUnitTest {
     @Before
     public void setup() {
         initMocks(this);
+        locationService = new LocationService(locationRepository, addressLevelTypeRepository, organisationRepository, locationMappingRepository, resetSyncService, organisationConfigService);
         addressLevelTypeController = new AddressLevelTypeController(addressLevelTypeRepository, locationService, projectionFactory, new AccessControlServiceStub(), organisationConfigService);
         AddressLevelType foo = new AddressLevelType();
         foo.setUuid(FOO_UUID);
@@ -51,8 +60,13 @@ public class AddressLevelTypeControllerUnitTest {
         bar.setName("bar");
         when(addressLevelTypeRepository.findByUuid(FOO_UUID)).thenReturn(foo);
         when(addressLevelTypeRepository.findByUuid(BAR_UUID)).thenReturn(bar);
-        when(addressLevelTypeRepository.findByName("foo")).thenReturn(foo);
-        when(addressLevelTypeRepository.findByName("bar")).thenReturn(bar);
+        when(addressLevelTypeRepository.findByNameIgnoreCaseAndIsVoidedFalse("foo")).thenReturn(foo);
+        when(addressLevelTypeRepository.findByNameIgnoreCaseAndIsVoidedFalse("FOO")).thenReturn(foo);
+        when(addressLevelTypeRepository.findByNameIgnoreCaseAndIsVoidedFalse("Foo")).thenReturn(foo);
+        when(addressLevelTypeRepository.findByNameIgnoreCaseAndIsVoidedFalse("bar")).thenReturn(bar);
+        when(addressLevelTypeRepository.findByNameIgnoreCaseAndIsVoidedFalse("BAR")).thenReturn(bar);
+        when(addressLevelTypeRepository.findByNameIgnoreCaseAndIsVoidedFalse("Bar")).thenReturn(bar);
+        when(addressLevelTypeRepository.findOne(1L)).thenReturn(foo);
     }
 
     @Test()
@@ -95,8 +109,56 @@ public class AddressLevelTypeControllerUnitTest {
         AddressLevelTypeContract create = new AddressLevelTypeContract();
         create.setUuid(UUID.randomUUID().toString());
         create.setName("tada");
+        create.setLevel(3d);
 
         ResponseEntity responseEntity = addressLevelTypeController.createAddressLevelType(create);
         assertThat(responseEntity.getStatusCodeValue(), is(equalTo(201)));
+    }
+
+    @Test()
+    public void shouldReturnErrorWhenOnCreateThereAlreadyExistsLocationTypeWithSameNameDifferentCase() throws Exception {
+        AddressLevelTypeContract create = new AddressLevelTypeContract();
+        create.setUuid(UUID.randomUUID().toString());
+        create.setName("FOO"); // Different case from existing "foo"
+
+        ResponseEntity responseEntity = addressLevelTypeController.createAddressLevelType(create);
+        assertThat(responseEntity.getStatusCodeValue(), is(equalTo(400)));
+        Map body = (Map) responseEntity.getBody();
+        assertThat(body.get("message"), is(equalTo("Location Type with name FOO already exists")));
+    }
+
+    @Test()
+    public void shouldReturnErrorWhenBatchSaveContainsDuplicateNames() throws Exception {
+        AddressLevelTypeContract validContract = new AddressLevelTypeContract();
+        validContract.setUuid(UUID.randomUUID().toString());
+        validContract.setName("valid");
+        validContract.setLevel(2d);
+
+        AddressLevelTypeContract duplicateContract = new AddressLevelTypeContract();
+        duplicateContract.setUuid(UUID.randomUUID().toString());
+        duplicateContract.setName("bar"); // Already exists
+        duplicateContract.setLevel(1d);
+
+        ResponseEntity responseEntity = addressLevelTypeController.save(java.util.Arrays.asList(validContract, duplicateContract));
+        assertThat(responseEntity.getStatusCodeValue(), is(equalTo(400)));
+        Map body = (Map) responseEntity.getBody();
+        assertThat(body.get("message"), is(equalTo("Location Type with name bar already exists")));
+    }
+
+    @Test
+    public void shouldUpdateWhenNameChangedWithDifferentCase() throws Exception {
+        AddressLevelTypeContract updateAddressLevelType = new AddressLevelTypeContract();
+        updateAddressLevelType.setUuid(FOO_UUID);
+        updateAddressLevelType.setName("FOO"); // Same name as original but different case
+        updateAddressLevelType.setLevel(1D);
+
+        ResponseEntity responseEntity = addressLevelTypeController.updateAddressLevelType(1L, updateAddressLevelType);
+        assertThat(responseEntity.getStatusCodeValue(), is(equalTo(201)));
+    }
+
+    @Test
+    public void shouldVoidAddressLevelTypeAndCleanupReferences() throws Exception {
+        ResponseEntity responseEntity = addressLevelTypeController.voidAddressLevelType(1L);
+        assertThat(responseEntity.getStatusCodeValue(), is(equalTo(200)));
     }
 }
