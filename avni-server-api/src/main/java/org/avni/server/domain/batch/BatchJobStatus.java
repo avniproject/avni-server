7af885cd0cf91a82df6e706cc447334b932b61ce
Joy A <itsjoy@gmail.com>
2025-07-03 14:48:27 +0530

    avniproject/avni-etl#136 | Include job start time in BatchJobStatus

--- avni-server-api/src/main/java/org/avni/server/domain/batch/BatchJobStatus.java
+++ avni-server-api/src/main/java/org/avni/server/domain/batch/BatchJobStatus.java
@@ -2,5 +2,5 @@ package org.avni.server.domain.batch;
 
 import java.util.Date;
 
-public record BatchJobStatus(String status, Date createDateTime, Date endDateTime, String exitMessage, String exitCode) {
+public record BatchJobStatus(String status, Date createDateTime, Date startDateTime, Date endDateTime, String exitMessage, String exitCode) {
 }
