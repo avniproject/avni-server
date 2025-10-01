f784bfc417a97ad931a93bca71f5fa480b3d8d5e
Om Bhardwaj <115864495+ombhardwajj@users.noreply.github.com>
2025-10-01 14:52:20 +0530

    #930 | Added copilot config to Configuration

--- avni-server-api/src/main/java/org/avni/server/web/util/ConfigurationResponse.java
+++ avni-server-api/src/main/java/org/avni/server/web/util/ConfigurationResponse.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class ConfigurationResponse {
     private List<ReportingSystem> reportingSystems;
+    private CopilotConfig copilotConfig;
 
     public List<ReportingSystem> getReportingSystems() {
         return reportingSystems;
@@ -13,5 +14,13 @@ public class ConfigurationResponse {
     public void setReportingSystems(List<ReportingSystem> reportingSystems) {
         List<ReportingSystem> filteredReportingSystems = reportingSystems.stream().filter(reportingSystem -> !(reportingSystem.getName().equals("") || reportingSystem.getUrl().equals("")) ).collect(Collectors.toList());
         this.reportingSystems = filteredReportingSystems;
+    }
+
+    public CopilotConfig getCopilotConfig() {
+        return copilotConfig;
+    }
+
+    public void setCopilotConfig(CopilotConfig copilotConfig) {
+        this.copilotConfig = copilotConfig;
     }
 }
