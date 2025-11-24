d4cadde0d72a96602893d0d3b8c03317525ab8c8
himeshr <himeshl@samanvayfoundation.org>
2025-11-24 18:11:09 +0530

    avniproject/avni-webapp#1641 | Move authentication from tasklet to job listener for template application

--- avni-server-api/src/main/java/org/avni/server/importer/batch/template/TemplateJobListener.java
+++ avni-server-api/src/main/java/org/avni/server/importer/batch/template/TemplateJobListener.java
@@ -1,11 +1,14 @@
 package org.avni.server.importer.batch.template;
 
+import org.avni.server.framework.security.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.BatchStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,9 +16,21 @@ import org.springframework.stereotype.Component;
 public class TemplateJobListener extends JobExecutionListenerSupport {
     private static final Logger logger = LoggerFactory.getLogger(TemplateJobListener.class);
 
+    @Value("#{jobParameters['userId']}")
+    private Long userId;
+    @Value("#{jobParameters['organisationUUID']}")
+    private String organisationUUID;
+    private final AuthService authService;
+
+    @Autowired
+    public TemplateJobListener(AuthService authService) {
+        this.authService = authService;
+    }
+
     @Override
     public void beforeJob(JobExecution jobExecution) {
         logger.info("Starting template application job");
+        authService.authenticateByUserId(userId, organisationUUID);
     }
 
     @Override
