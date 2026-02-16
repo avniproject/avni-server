8acb8719b79d173edc9db8d7ac654902ff83d0f5
Om Bhardwaj <115864495+ombhardwajj@users.noreply.github.com>
2026-02-16 16:47:14 +0530

    avniproject/avni-client#1841| Add guideUserToRegisterButton Key

--- avni-server-data/src/main/java/org/avni/server/application/OrganisationConfigSettingKey.java
+++ avni-server-data/src/main/java/org/avni/server/application/OrganisationConfigSettingKey.java
@@ -3,5 +3,5 @@ package org.avni.server.application;
 public enum OrganisationConfigSettingKey {
     languages, searchFilters, myDashboardFilters, lowestAddressLevelType, saveDrafts, enableComments,
     searchResultFields, useMinioForStorage, useKeycloakAsIDP, skipRuleExecution, customRegistrationLocations, enableMessaging,
-    donotRequirePasswordChangeOnFirstLogin, failOnValidationError
+    donotRequirePasswordChangeOnFirstLogin, failOnValidationError, guideUserToRegisterButton
 }
